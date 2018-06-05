35294554458c9d1a37c518d274461c9015067c27
Yusuke Hayashi <yusuke8h@gmail.com>
2018-06-05 16:40:42 +0900

    Implement api server

---
+++ server/query.js
@@ -0,0 +1,32 @@
+const db = require("../db");
+
+const getUser = (userId) => db("users").where("id", userId);
+
+const addUser = (userId) => db("users").insert({ id: userId });
+const getRallies = () => db("rallies");
+const getRalliesOfUser = (userId) =>
+  db("rallies")
+    .innerJoin("rallies_to_users", "rallies.id", "rallies_to_users.rally_id")
+    .where("user_id", userId);
+const getLocations = (userId, rallyId) =>
+  db("locations")
+    .where("rally_id", rallyId)
+    .innerJoin(
+      "locations_to_users",
+      "locations.id",
+      "locations_to_users.user_id"
+    )
+    .where("user_id", userId);
+const doneLocation = (userId, locationId) =>
+  db("locations")
+    .where("id", locationId)
+    .update("visited", true);
+
+module.exports = {
+  getUser,
+  addUser,
+  getRallies,
+  getRalliesOfUser,
+  getLocations,
+  doneLocation,
+};
