d317e65d56abb7b5e465062e787edfd4163c7581
morita657 <morita657@gmail.com>
2018-06-14 11:05:38 +0900

    Create basic tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 export default class App extends Component {
   render() {
-    return <div>hi</div>;
+    return (
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    );
+    // <div>Hello World</div>;
   }
 }
