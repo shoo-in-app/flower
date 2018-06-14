37d7d0b6aebe903a836d765059c93663a366431c
Yusuke Hayashi <yusuke8h@gmail.com>
2018-06-14 12:28:03 +0900

    Add new column to rallies table

--- db/migrations/20180605153835_add_rallies_table.js
+++ db/migrations/20180605153835_add_rallies_table.js
@@ -3,6 +3,9 @@ exports.up = (knex) =>
     table.increments("id");
     table.string("title", 255);
     table.text("description");
+    table.integer("user_count");
+    table.dateTime("start_datetime");
+    table.dateTime("end_datetime");
   });
 
 exports.down = (knex) => knex.schema.dropTable("rallies");
