a1bc5c82426c3854205af927484e5ae268ec7874
morita657 <morita657@gmail.com>
2018-06-18 14:01:12 +0900

    Create another mock data

--- src/List.jsx
+++ src/List.jsx
@@ -19,6 +19,19 @@ export default class List extends Component {
               lng: 100.47180978252506,
             },
           ],
+        }, {
+          title: "test",
+          description: "test",
+          start_datetime: "2018-06-19T14:59:00.000Z",
+          end_datetime: "2018-06-21T13:56:00.000Z",
+          locations: [
+            {
+              name: "test",
+              description: "test",
+              lat: 227.4066325,
+              lng: 100.47180978252506,
+        },
+      ],
         },
       ],
     };
