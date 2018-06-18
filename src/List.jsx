54847b730eba23bacbb62c9372a016cdc87611dd
morita657 <morita657@gmail.com>
2018-06-18 14:01:49 +0900

    Implement to handle multi list

--- src/List.jsx
+++ src/List.jsx
@@ -19,7 +19,8 @@ export default class List extends Component {
               lng: 100.47180978252506,
             },
           ],
-        }, {
+        },
+        {
           title: "test",
           description: "test",
           start_datetime: "2018-06-19T14:59:00.000Z",
@@ -30,8 +31,8 @@ export default class List extends Component {
               description: "test",
               lat: 227.4066325,
               lng: 100.47180978252506,
-        },
-      ],
+            },
+          ],
         },
       ],
     };
@@ -49,10 +50,13 @@ export default class List extends Component {
   render() {
     let rallies;
     if (this.state.rallies.length > 0) {
-      const rallyList = this.state.rallies.map((rally, index) => (
-        <li key={index}>{rally}</li>
-      ));
-      rallies = <ul>{JSON.stringify(rallyList[0].props.children)}</ul>;
+      rallies = (
+        <ul>
+          {this.state.rallies.map((rally, index) => (
+            <li key={index}>{JSON.stringify(rally)}</li>
+          ))}
+        </ul>
+      );
     } else {
       rallies = <p>You have not created any rallies yet.</p>;
     }
