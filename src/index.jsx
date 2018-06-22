e75f50e2c92856279d78f5df9d7c43e70b254d41
morita657 <morita657@gmail.com>
2018-06-22 11:14:31 +0900

    Add images

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./images/favicon.ico";
 import "./styles/style.css";
-import "./images/user.svg";
+
 ReactDOM.render(<App />, document.getElementById("root"));
