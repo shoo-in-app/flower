8ee7109ec858238798f9855b4737322ae775c605
Yusuke Hayashi <yusuke8h@gmail.com>
2018-06-25 17:17:03 +0900

    Add logo to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,11 +4,13 @@ import Card from "@material-ui/core/Card";
 import GoogleButton from "react-google-button";
 import android from "../images/android.png";
 import ios from "../images/ios.svg";
+import logo from "../images/prep-white.png";
 
 const Login = () => {
   return (
     <Card className={style.login}>
-      <h1 style={{ color: "white" }}>Shoo-in</h1>
+      {/* <h1 style={{ color: "white" }}>Shoo-in</h1> */}
+      <img src={logo} alt={"logo"} style={{ margin: "auto", width: "50%" }} />
       <p style={{ color: "white" }}>Welcome to the rally creation page</p>
 
       <a href="/auth/google" style={{ textDecoration: "none" }}>
@@ -17,8 +19,14 @@ const Login = () => {
 
       <p style={{ color: "white" }}>Download mobile App</p>
       <div>
-        <img src={android} alt={"android"} className={style.download} />
-        <img src={ios} alt={"ios"} className={style.download} />
+        <a
+          href={
+            "https://play.google.com/store/apps/details?id=com.shooin.stamprally"
+          }
+        >
+          <img src={android} alt={"android"} style={{ width: "200px" }} />
+        </a>
+        <img src={ios} alt={"ios"} style={{ width: "176px" }} />
       </div>
     </Card>
   );
